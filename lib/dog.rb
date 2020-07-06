e5104eeb88b9ef736e92b157ba833fbcd8c7eec3
Gustavo Hernandez <gnetboy.y@gmail.com>
2020-07-06 20:07:21 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,24 @@
 # Add your code here
+class Dog
+    attr_accessor :name
+    @@all=[]
+    def initialize(name)
+        @name=name
+        self
+        @@all << self
+        save
+    end
+    def self.all
+       return @@all.uniq
+    end
+    def self.clear_all
+        @@all.clear
+    end
+    def self.print_all
+       puts  @@all.collect{|dog| dog.name}.uniq
+    end
+    def save
+        @@all << self
+    end
+
+end
